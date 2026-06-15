83bf86d603d67241595231de9d22f0f2aa8738bb
jainiksha <jainiksha.patel2007@gmail.com>
2026-06-15 18:02:48 +0530

    fix: remove duplicate React import

--- frontend/src/components/portfolio/templates/Scandinavian_Light/index.jsx
+++ frontend/src/components/portfolio/templates/Scandinavian_Light/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import data from '../../../../data/dummy_data.json';
+import dummyData from '../../../../data/dummy_data.json';
 import Hero from './Hero';
 import About from './About';
 import Skills from './Skills';
@@ -12,7 +12,6 @@ const FONT_STYLESHEET_ID = 'scandinavian-light-fonts';
 const FONT_HREF =
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Lora:wght@400;500;600&display=swap';
 import { usePortfolio } from "../../../../context/PortfolioContext";
-import React from 'react';
 
 export default function ScandinavianLight() {
   useEffect(() => {
